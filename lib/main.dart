8ba9ddb34e7471ca7832e1ead334442ff650ee76
wolla22 <95152638+wolla22@users.noreply.github.com>
2023-02-25 12:11:50 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -84,3 +84,282 @@ class _LandingPageState extends State<LandingPage> {
     );
   }
 }
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final _formKey = GlobalKey<FormState>();
+  bool _isCompany = true;
+  String? _companyName;
+  String? _email;
+  String? _password;
+
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your email';
+    } else if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+      return 'Please enter a valid email';
+    }
+    return null;
+  }
+
+  String? _validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your password';
+    } else if (value.length < 6) {
+      return 'Password must be at least 6 characters';
+    }
+    return null;
+  }
+
+  void _handleRadioValueChange(bool? value) {
+    setState(() {
+      _isCompany = value ?? true;
+    });
+  }
+
+  void _handleCompanyNameChange(String value) {
+    setState(() {
+      _companyName = value;
+    });
+  }
+
+  void _handleSignUp() {
+    if (_formKey.currentState!.validate()) {
+      // Create a Map object to hold the input data
+      Map<String, dynamic> userData = {
+        'email': _email,
+        'password': _password,
+      };
+
+      if (_isCompany && _companyName!.isNotEmpty) {
+        userData['companyName'] = _companyName;
+      }
+
+      // Convert the Map to JSON format
+      String jsonData = json.encode(userData);
+
+      // TODO: Send jsonData to the database API
+
+      // Navigate to dashboard page
+      Navigator.pushNamed(context, '/user');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Sign up'),
+        ),
+        body: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            offset: Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            'Sign up',
+                            style: TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 16.0),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                            validator: _validateEmail,
+                          ),
+                          SizedBox(height: 16.0),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                            obscureText: true,
+                            validator: _validatePassword,
+                          ),
+                          SizedBox(height: 16.0),
+                          Text('I am a:'),
+                          Row(
+                            children: [
+                              Radio(
+                                value: true,
+                                groupValue: _isCompany,
+                                onChanged: _handleRadioValueChange,
+                              ),
+                              Text('Company'),
+                            ],
+                          ),
+                          if (_isCompany)
+                            TextField(
+                              decoration: InputDecoration(
+                                labelText: "What is your company's name?",
+                              ),
+                              onChanged: _handleCompanyNameChange,
+                            ),
+                          Row(
+                            children: [
+                              Radio(
+                                value: false,
+                                groupValue: _isCompany,
+                                onChanged: _handleRadioValueChange,
+                              ),
+                              Text('Job searcher'),
+                            ],
+                          ),
+                          SizedBox(height: 16.0),
+                          ElevatedButton(
+                            onPressed: _handleSignUp,
+                            child: Text('Sign up'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ]
+              ),
+            ))
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your email';
+    } else if (!RegExp(r'^[\w-.]+@([\w-]+.)+[\w-]{2,4}$').hasMatch(value)) {
+      return 'Please enter a valid email';
+    }
+    return null;
+  }
+
+  String? _validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your password';
+    } else if (value.length < 6) {
+      return 'Password must be at least 6 characters';
+    }
+    return null;
+  }
+
+  void _handleLogin() {
+    if (_formKey.currentState!.validate()) {
+// Perform login
+// ...
+      // Navigate to dashboard page
+      Navigator.pushNamed(context, '/company');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 16.0),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        validator: _validateEmail,
+                      ),
+                      SizedBox(height: 16.0),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        obscureText: true,
+                        validator: _validatePassword,
+                      ),
+                      SizedBox(height: 16.0),
+                      ElevatedButton(
+                        onPressed: _handleLogin,
+                        child: Text('Login'),
+                      ),
+                    ],
+                  ),
+                ),
+              ]),
+        ),
+      ),
+    );
+  }
+}
+
+
+
