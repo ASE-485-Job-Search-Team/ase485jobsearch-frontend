24fb451106dc6b5c41befe6818dd79289283e928
Jyhdel Pamonag <jyjypamonag@gmail.com>
2023-04-12 22:53:27 -0400

    Update Api class

--- lib/constants/api.dart
+++ lib/constants/api.dart
@@ -1,6 +1,9 @@
 class Api {
+  static const String baseUrl = 'http://localhost:5050/api';
+  static const String jobPostingsUrl = '$baseUrl/jobs/job-postings';
+
   static const String appName = "JobHive";
-  static const String baseUrl = 'localhost:3000';
+  static const String authUrl = 'localhost:3000';
   static const String loginAPI = "/api/auth/login";
   static const String registerAPI = "/api/auth/register/user";
   static const String registerCompnayAPI = "/api/auth/register/admin";
