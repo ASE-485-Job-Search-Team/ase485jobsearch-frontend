07929a446ca3375b8f77327e4dd08469d33d9df3
Trung Cao <caot1@nku.edu>
2023-04-22 14:19:57 -0400

    Replace company ID

--- lib/screens/my_applications/my_job_postings.dart
+++ lib/screens/my_applications/my_job_postings.dart
@@ -12,6 +12,8 @@ import 'package:jobsearchmobile/screens/my_applications/widgets/create_app_butto
 import 'package:http/http.dart' as http;
 import 'package:jobsearchmobile/screens/auth/widgets/resume_upload_button.dart';
 import '../../constants/api.dart';
+import '../../models/user.dart';
+import '../../services/auth_api_service.dart';
 import '../home/widgets/job_info_item.dart';
 
 class MyJobPostings extends StatefulWidget {
@@ -22,10 +24,45 @@ class MyJobPostings extends StatefulWidget {
 }
 
 class _MyJobPostingsState extends State<MyJobPostings> {
-  Future<List<JobPosting>> fetchJobPostingForBuilder(
+  late Future<User> _userFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _userFuture = _loadUserData();
+  }
+
+  Future<User> _loadUserData() async {
+    String response = await APIService.getUserProfile();
+
+    final model = jsonDecode(response);
+    final isAdmin = model['data']['isAdmin'];
+    User user;
+
+    if (isAdmin) {
+      user = User(
+        id: model['data']['id'],
+        name: model['data']['company'],
+        email: model['data']['email'],
+        isAdmin: true,
+      );
+    } else {
+      user = User(
+        id: model['data']['id'],
+        name: model['data']['first'] + ' ' + model['data']['last'],
+        email: model['data']['email'],
+        resume: model['data']['resume'],
+        isAdmin: false,
+      );
+    }
+
+    return user;
+  }
+
+  Future<List<JobPosting>> fetchJobPostingForBuilder(String id,
       {String query = ''}) async {
-    final response = await http.get(Uri.parse(
-        '${Api.baseUrl}/companies/nR3nWouQJIR3fgyIggHP/jobs?$query')); //TODO: Replace with real id
+    final response =
+        await http.get(Uri.parse('${Api.baseUrl}/companies/$id/jobs?$query'));
     if (response.statusCode == 200) {
       final List<dynamic> jsonData = jsonDecode(response.body);
       return jsonData
@@ -52,74 +89,92 @@ class _MyJobPostingsState extends State<MyJobPostings> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-          top: MediaQuery.of(context).padding.top + 16, left: 16, right: 16),
-      child: Column(
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
+    return FutureBuilder<User>(
+        future: _userFuture,
+        builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else if (!snapshot.hasData) {
+            return const Text('No data');
+          } else {
+            final user = snapshot.data;
+            return Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).padding.top + 16,
+                  left: 16,
+                  right: 16),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Text(
-                    'Job Postings',
-                    style:
-                        TextStyle(fontSize: 22.0, fontWeight: FontWeight.w600),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Job Postings',
+                            style: TextStyle(
+                                fontSize: 22.0, fontWeight: FontWeight.w600),
+                          ),
+                          const SizedBox(
+                            height: 16.0,
+                          ),
+                          FutureBuilder(
+                              future: fetchJobPostingForBuilder(user!.id),
+                              builder: (context, snapshot) {
+                                if (snapshot.hasData) {
+                                  final jobPostings =
+                                      snapshot.data as List<JobPosting>;
+                                  final jobPostingsByDate =
+                                      mapByDate(jobPostings);
+                                  return Column(
+                                    children: jobPostingsByDate.entries
+                                        .map((jobPosting) {
+                                      return Padding(
+                                        padding: EdgeInsets.only(bottom: 16.0),
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              DateFormat.yMMMMd()
+                                                  .format(jobPosting.key)
+                                                  .toUpperCase(),
+                                              textAlign: TextAlign.left,
+                                              style: TextStyle(
+                                                fontSize: 16.0,
+                                                fontWeight: FontWeight.w500,
+                                                color: Colors.grey.shade500,
+                                              ),
+                                            ),
+                                            const Divider(),
+                                            Column(
+                                              children: jobPosting.value
+                                                  .map((jobPosting) {
+                                                return JobPostingItem(
+                                                  jobPosting: jobPosting,
+                                                );
+                                              }).toList(),
+                                            ),
+                                          ],
+                                        ),
+                                      );
+                                    }).toList(),
+                                  );
+                                } else {
+                                  return const Center(
+                                      child: CircularProgressIndicator());
+                                }
+                              }),
+                        ],
+                      ),
+                    ),
                   ),
-                  const SizedBox(
-                    height: 16.0,
-                  ),
-                  FutureBuilder(
-                      future: fetchJobPostingForBuilder(),
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData) {
-                          final jobPostings = snapshot.data as List<JobPosting>;
-                          final jobPostingsByDate = mapByDate(jobPostings);
-                          return Column(
-                            children:
-                                jobPostingsByDate.entries.map((jobPosting) {
-                              return Padding(
-                                padding: EdgeInsets.only(bottom: 16.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      DateFormat.yMMMMd()
-                                          .format(jobPosting.key)
-                                          .toUpperCase(),
-                                      textAlign: TextAlign.left,
-                                      style: TextStyle(
-                                        fontSize: 16.0,
-                                        fontWeight: FontWeight.w500,
-                                        color: Colors.grey.shade500,
-                                      ),
-                                    ),
-                                    const Divider(),
-                                    Column(
-                                      children:
-                                          jobPosting.value.map((jobPosting) {
-                                        return JobPostingItem(
-                                          jobPosting: jobPosting,
-                                        );
-                                      }).toList(),
-                                    ),
-                                  ],
-                                ),
-                              );
-                            }).toList(),
-                          );
-                        } else {
-                          return const Center(
-                              child: CircularProgressIndicator());
-                        }
-                      }),
                 ],
               ),
-            ),
-          ),
-        ],
-      ),
-    );
+            );
+          }
+        });
   }
 }
