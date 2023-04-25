ea46bc4393b4f99427339b550d50d480f7852cdf
Trung Cao <caot1@nku.edu>
2023-04-24 23:12:06 -0400

    Finish company integration

--- lib/services/job_posting_service.dart
+++ lib/services/job_posting_service.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../constants/api.dart';
-import '../models/job_info.dart'; // Import the JobPosting data model
+import '../models/job_info.dart';
+import '../models/job_posting_request.dart'; // Import the JobPosting data model
 
 class JobPostingService {
   final http.Client httpClient;
@@ -11,7 +12,7 @@ class JobPostingService {
   Future<List<JobPosting>> fetchJobPostingsForBuilder(http.Client client,
       {String query = ''}) async {
     final response =
-    await client.get(Uri.parse('${Api.jobPostingsUrl}?$query'));
+        await client.get(Uri.parse('${Api.jobPostingsUrl}?$query'));
     if (response.statusCode == 200) {
       final jobPostings = jsonDecode(response.body) as List;
       return jobPostings
@@ -22,46 +23,41 @@ class JobPostingService {
     }
   }
 
-  Future<http.Response> createJobPosting(
+  Future<http.Response> createJobPosting(http.Client client,
       {required String title,
-        required companyId,
-        required String location,
-        required String jobType,
-        required String description,
-        required List<String> qualifications,
-        required List<String> responsibilities,
-        required String datePosted,
-        required String dateClosing,
-        required String salaryRange}) async {
-        var url = Uri.parse('${Api.jobPostingsUrl}/create');
+      required companyId,
+      required String location,
+      required String jobType,
+      required String description,
+      required List<String> qualifications,
+      required List<String> responsibilities,
+      required String datePosted,
+      required String dateClosing,
+      required String salaryRange}) async {
+    var url = Uri.parse('${Api.baseUrl}/jobs/create');
 
-        Map<String, String> headers = {
-          'Content-type': 'application/json',
-          'Accept': 'application/json',
-        };
+    Map<String, String> headers = {
+      'Content-type': 'application/json',
+      'Accept': 'application/json',
+    };
 
-        // Create a JobPosting object
-        JobPosting jobPosting = JobPosting(
-          id: '', // You may need to assign an id before sending to the API
-          title: title,
-          companyId: companyId, // Replace with companyId
-          company: '', // Replace with company name
-          location: location,
-          jobType: jobType,
-          description: description,
-          qualifications: qualifications,
-          responsibilities: responsibilities,
-          datePosted: DateTime.parse(datePosted),
-          dateClosing: DateTime.parse(dateClosing),
-          companyLogo: '', // Replace with companyLogo URL
-          salaryRange: salaryRange,
-        );
+    // Create a JobPosting object
+    JobPostingRequest jobPostingRequest = JobPostingRequest(
+      title: title,
+      companyId: companyId,
+      location: location,
+      jobType: jobType,
+      description: description,
+      qualifications: qualifications,
+      responsibilities: responsibilities,
+      datePosted: DateTime.parse(datePosted),
+      dateClosing: DateTime.parse(dateClosing),
+      salaryRange: salaryRange,
+    );
 
-        // Use the JobPosting object's toJson method to generate the request body
-        final response = await http.post(url,
-            headers: headers, body: jsonEncode(jobPosting.toJson()));
-
-        return response;
+    // Use the JobPosting object's toJson method to generate the request body
+    final response = await client.post(url,
+        headers: headers, body: jsonEncode(jobPostingRequest.toJson()));
+    return response;
   }
 }
-
